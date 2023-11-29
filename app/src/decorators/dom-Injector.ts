9c106b0ea872d51004d9eba50b0d39aea83810cc
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-28 23:01:47 -0300

    commit 6

--- app/src/decorators/dom-Injector.ts
+++ app/src/decorators/dom-Injector.ts
@@ -4,8 +4,12 @@ export function domInjector(seletor: string) {
     target: any, // Pode ser um construtor ou prototype da classe
     propertyKey: string,
   ) {
+    let elemento: HTMLElement;
     const getter = function() {
-      const elemento = document.querySelector(seletor);
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(seletor);
+      }
+      // const elemento = document.querySelector(seletor);
       return elemento;
     };
 
